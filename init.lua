43df23c565f1918ed2e2825c56420540e7f118f2
Lukino <LukinoProg@gmail.com>
2024-12-26 14:19:19 +0100

    Add vtsls that adds typescript support

--- init.lua
+++ init.lua
@@ -627,7 +627,20 @@ require('lazy').setup({
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
         --
-
+        vtsls = {
+          settings = {
+            typescript = {
+              inlayHints = {
+                parameterNames = { enabled = 'literals', suppressWhenArgumentMatchesName = false },
+                parameterTypes = { enabled = true },
+                variableTypes = { enabled = true, suppressWhenArgumentMatchesName = false },
+                propertyDeclarationTypes = { enabled = true },
+                functionLikeReturnTypes = { enabled = true },
+                enumMemberValues = { enabled = true },
+              },
+            },
+          },
+        },
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
