f0806279c79497cbe362172fc9c71a2c90ee049c
Lukino <LukinoProg@gmail.com>
2024-12-30 21:32:07 +0100

    Add <C-s> to save files

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,8 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+
+vim.keymap.set('n', '<C-s>', '<Esc>:w<CR>')
+vim.keymap.set('i', '<C-s>', '<Esc>:w<CR>')
+
 return {}
